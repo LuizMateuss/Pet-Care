b0cb305ced46b4438ea9cd6d78e443e3c3efb544
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2022-08-29 10:10:37 -0300

    Alterado estilização e adicionado modal em contratar cuidador

--- src/screens/ContractService.js
+++ src/screens/ContractService.js
@@ -1,11 +1,44 @@
 import { HStack, VStack, View, Text, Image } from 'native-base'
+import { Modal, TouchableOpacity } from 'react-native'
+import { useState, useEffect } from 'react'
 import { ButtonMain } from '../components/ButtonMain'
 import { RatingBar } from '../components/RatingStar'
-import { PawPrint } from 'phosphor-react-native'
+import { CaretLeft, PawPrint } from 'phosphor-react-native'
 import { ServiceButton } from '../components/ServiceButton'
 import { useNavigation } from '@react-navigation/native'
 
+/*
+  Tela contratar cuidador:
+  -> Componente ModalCancel:
+    -> showModal (bool), valor booleano, de acordo com a propriedade passada (visible), por padrão sempre é definido false.
+    -> setShowModal (function), função responsável por alterar o valor do showModal.
+    -> useEffect(), responsável por chamar a função toggleModal() sempre que a variável visible ter uma alteração.
+*/
+
+const ModalCancel = ({ visible, children, ...props }) => {
+  const [showModal, setShowModal] = useState(props.visible)
+  useEffect(() => {
+    toggleModal()
+  }, [visible])
+  const toggleModal = () => {
+    visible ? setShowModal(true) : setShowModal(false)
+  }
+  return (
+    <Modal transparent visible={showModal}>
+      <View
+        flex={1}
+        bg="rgba(217,217,217,0.8)"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {children}
+      </View>
+    </Modal>
+  )
+}
+
 export function ContractService({ route }) {
+  const [visible, setVisible] = useState(false)
   const navigation = useNavigation()
   const { isCare } = route.params
   return (
@@ -32,17 +65,18 @@ export function ContractService({ route }) {
         </VStack>
       </HStack>
 
-      <VStack mt={10}>
-        <Text textAlign="center" fontSize={20}>
-          Informações do passeio:
-        </Text>
+      <VStack mt={5}>
         <Text textAlign="center" fontSize={20}>
           Valor: R$40,00
         </Text>
         <Text textAlign="center" fontSize={20}>
-          Selecione sua forma de pagamento:{' '}
+          Selecione o método de pagamento
         </Text>
-        <ButtonMain title="Pagamento" colorText="white" color="#511AC7" />
+        <ButtonMain title="Pix" colorText="white" color="#511AC7" />
+        <Text textAlign="center" w="80%" mx="auto" my={2}>
+          Obs.: O valor será retido pelo aplicativo e será repassado ao cuidador
+          após o serviço ser concluído.
+        </Text>
       </VStack>
 
       <VStack
@@ -75,13 +109,34 @@ export function ContractService({ route }) {
           <ServiceButton
             title="Sim"
             color="#511AC7"
-            nextPage={() => navigation.navigate('startService', { isCare })}
+            nextPage={() => setVisible(true)}
           />
         </View>
         <View w="40%" mx={2}>
-          <ServiceButton title="Não" color="#BC0000" />
+          <ServiceButton
+            title="Não"
+            nextPage={() => navigation.goBack()}
+            color="#BC0000"
+          />
         </View>
       </HStack>
+      <ModalCancel visible={visible}>
+        <View bg="primary.700" w="80%" p={4} borderRadius={20}>
+          <TouchableOpacity
+            onPress={() => {
+              setVisible(false)
+              navigation.navigate('menuHamburguer', { isCare })
+            }}
+          >
+            <CaretLeft size={20} color="#FFF" />
+          </TouchableOpacity>
+          <Text color="white" textAlign="center" my={5}>
+            Serviço agendado com sucesso! Verifique com detalhes em “Serviços
+            agendados”. Lembre-se de conversar com o cuidador para combinar como
+            entregar o animal.
+          </Text>
+        </View>
+      </ModalCancel>
     </View>
   )
 }
