e1b9ed9f7b10be5779aa018c318c0dd493218c5b
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-09-15 22:58:54 -0300

    corrige erro do insert, implementa cadastro básico na api

--- API/db/index.php
+++ API/db/index.php
@@ -17,9 +17,9 @@
     }
     
     // rotas
+    $app->get( '/usuario', 'getUsuario');
     $app->map(['get', 'post'], '/login/{email}/{password}/{isCare}', 'getLogin');
-    $app->map(['get', 'post'], '/registration', 'getRegistration');
-    ///{name}/{email}/{password}/{birthday}/{phone}/{isCare}
+    $app->map(['get', 'post'], '/registration/{name}/{email}/{password}/{birthday}/{phone}/{isCare}', 'getRegistration');
 
     //FUNÇÕES DE CONCÇÃO
     
@@ -28,14 +28,15 @@
         return $response;
     });
 
-    // function getCuidadores(Request $request, Response $response, array $args){
-    //     $sql = "SELECT * FROM Cuidador";
-    //     $stmt = getConn()->query($sql);
-    //     $cuidadores = $stmt->fetchAll(PDO::FETCH_OBJ);
-    //     $response->getBody()->write(json_encode($cuidadores));
-    //     // $response->json_encode($cuidadores);
-    //     return $response;
-    // }
+    function getUsuario(Request $request, Response $response, array $args){
+        //para teste com o banco
+        $sql = "SELECT cd_usuario, nm_usuario FROM usuario";
+        $stmt = getConn()->query($sql);
+        $usuario = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $response->getBody()->write(json_encode($usuario));
+        // $response->json_encode($usuario);
+        return $response;
+    }
 
     function getLogin(Request $request, Response $response, array $args){
         $email = $args['email'];
@@ -56,58 +57,28 @@
     }
 
     function getRegistration(Request $request, Response $response, array $args){
-        //https://discourse.slimframework.com/t/insert-data-in-db-with-form-using-slim/3003/3
-        //https://discourse.slimframework.com/t/how-to-send-post-data-in-a-crud-class/3515
-        //https://odan.github.io/2017/01/07/basic-crud-operations-with-pdo.html
-         // Get POST data
-        $post = (array)$request->getParsedBody();
-
-        $row = [
-            'nome' => "tetse2",//$post['nome'],
-            'email' => "luiz@luiz.ll",//$post['email'],
-            'senha' => "123" //$post['senha']
-        ];
-
-        $sql = "INSERT INTO usuario SET nm_usuario=:nome, nm_email=:email, nm_senha:senha";
-
-        /** @var PDO $pdo */
-        // $pdo = $this->get(PDO::class);
+        $name = $args['name'];
+        $email = $args['email'];
+        $password = $args['password'];
+        $birthday = $args['birthday'];
+        $phone = $args['phone'];
+        $isCare = $args['isCare'];
         $conn = getConn();
+
+        $sql = "INSERT INTO usuario 
+                     SET nm_usuario=:nm_usuario, nm_email=:nm_email, nm_senha=:nm_senha, dt_data=:dt_data, cd_telefone=:cd_telefone, cd_isCare=:cd_isCare";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam("nm_usuario", $row['nome']);
-        $stmt->bindParam("nm_email", $row['email']);
-        $stmt->bindParam("nm_senha", $row['senha']);
-        // $smt->execute();
-        // $success = $pdo->prepare($sql);//->execute($row);
-
-    return $response->withJson(['success' => $row]);
-
-
-
-        // $data = (array)$request->getParsedBody();
-
-        // $name = $data[$args['name']];
-        // $email = $data[$args['email']];
-        // $password = $data[$args['password']];
-        // $birthday = $data[$args['birthday']];
-        // $phone = $data[$args['phone']];
-        // $isCare = $data[$args['isCare']];
-
-        // $conn = getConn();
-        // $sql = "INSERT INTO usuario 
-        //             SET nm_usuario=:nm_usuario, nm_email=:nm_email, nm_senha:nm_senha, dt_data=:dt_data, cd_telefone=:cd_telefone, cd_isCare=:cd_isCare";
-        // $stmt = $conn->prepare($sql);
-        // $stmt->bindParam("nm_usuario", $name);
-        // $stmt->bindParam("nm_email", $email);
-        // $stmt->bindParam("nm_senha", $password);
-        // $stmt->bindParam("dt_data", $birthday);
-        // $stmt->bindParam("cd_telefone", $phone);
-        // $stmt->bindParam("cd_isCare", $isCare);
-        // $stmt->execute();
-        // // $informations=$stmt->fetchObject();
-        // // $response->getBody()->write(json_encode($informations));
-        // $response = "oi";
-        // return $response;
+        $stmt->bindParam("nm_usuario", $name);
+        $stmt->bindParam("nm_email", $email);
+        $stmt->bindParam("nm_senha", $password);
+        $stmt->bindParam("dt_data", $birthday);
+        $stmt->bindParam("cd_telefone", $phone);
+        $stmt->bindParam("cd_isCare", $isCare);
+        $stmt->execute();
+        
+        $message = "Cadastrado";
+        $response->getBody()->write(json_encode($message));
+        return $response;
     }
 
     $app->run();
