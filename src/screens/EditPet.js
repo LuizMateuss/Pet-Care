6be34528163209b47238edb9c98e3f4de97f5dd9
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2022-09-22 23:40:11 -0300

    Tela editar animal

--- src/screens/EditPet.js
+++ src/screens/EditPet.js
@@ -1,5 +1,6 @@
 import { VStack, Text, Image, HStack } from 'native-base'
 import { TouchableOpacity } from 'react-native'
+import { Button } from '../components/Button'
 import { Header } from '../components/Header'
 import { Input } from '../components/Input'
 
@@ -39,6 +40,44 @@ export function EditPet({ route }) {
         </Text>
         <Input w="60%" borderWidth={1} borderColor="#511AC7" />
       </HStack>
+      <VStack bg="primary.700" p={4} borderRadius={10} w="95%" mx="auto" mt={4}>
+        <Text fontWeight="black" textAlign="center" fontSize={16} color="white">
+          Alterar informações do animal:
+        </Text>
+        <HStack alignItems="center" justifyContent="space-between">
+          <Text fontWeight="black" fontSize={16} color="white">
+            Peso aproximado:
+          </Text>
+          <Input placeholder="kg" w="40%" />
+        </HStack>
+        <VStack>
+          <Text
+            fontWeight="black"
+            textAlign="center"
+            fontSize={16}
+            color="white"
+          >
+            Descrição do animal (opcional):
+          </Text>
+          <Input />
+        </VStack>
+      </VStack>
+      <Button
+        mt={10}
+        title="Excluir animal"
+        w="80%"
+        color="red.700"
+        borderWidth={1}
+        borderColor="red.700"
+      />
+      <Button
+        mt={4}
+        title="Salvar alterações"
+        w="80%"
+        color="primary.700"
+        borderWidth={1}
+        borderColor="primary.700"
+      />
     </VStack>
   )
 }
