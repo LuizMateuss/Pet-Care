dee791e8713f2446d3c8c34492ce816287ddd1d3
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2022-09-24 14:46:56 -0300

    Recebe usuário na rota

--- src/screens/RequestedServices.js
+++ src/screens/RequestedServices.js
@@ -6,7 +6,7 @@ import { RequestedServiceCard } from '../components/RequestedServiceCard'
 export function RequestedServices({ route }) {
   const navigation = useNavigation()
 
-  const { isCare } = route.params
+  const { isCare, user } = route.params
   const mainColor = isCare ? '#00ABBC' : '#511AC7'
   return (
     <VStack>
