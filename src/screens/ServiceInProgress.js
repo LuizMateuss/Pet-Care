3e4bdc5272709112cda687b21e260bba3174635a
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2022-10-05 19:12:57 -0300

    Implementado API Maps

--- src/screens/ServiceInProgress.js
+++ src/screens/ServiceInProgress.js
@@ -1,10 +1,10 @@
 import { Image, Text, VStack, View, useTheme } from 'native-base'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import { PawPrint } from 'phosphor-react-native'
 import { Button } from '../components/Button'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import MapViewDirections from 'react-native-maps-directions'
 
 export function ServiceInProgress({ route }) {
@@ -12,21 +12,19 @@ export function ServiceInProgress({ route }) {
     pickupAndDropCords: {
       latitude: -23.966185579866277,
       longitude: -46.337672487834844,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221
     },
     currentCords: {
       latitude: -23.97590347583942,
       longitude: -46.31840498403957,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221
     }
   })
-
+  const mapRef = useRef()
   const { pickupAndDropCords, currentCords } = initialCords
-  // const { isCare, user } = route.params
-  const isCare = false
-  const user = { name: 'Jorge' }
+  const { isCare, user } = route.params
   const mainColor = isCare ? '#00ABBC' : '#511AC7'
   const { colors } = useTheme()
   const navigation = useNavigation()
@@ -34,6 +32,7 @@ export function ServiceInProgress({ route }) {
     <SafeAreaView style={{ backgroundColor: '#fff', flex: 1 }}>
       <VStack bg="white">
         <MapView
+          ref={mapRef}
           initialRegion={pickupAndDropCords}
           style={{
             width: '100%',
@@ -42,10 +41,30 @@ export function ServiceInProgress({ route }) {
             zIndex: 0
           }}
         >
+          <Marker coordinate={pickupAndDropCords}>
+            <Image
+              alt="Ícone local"
+              source={require('../../assets/img/pinPurple.png')}
+              w="25"
+              h="25"
+              resizeMode="contain"
+            />
+          </Marker>
+          <Marker coordinate={currentCords}>
+            <Image
+              alt="Ícone local"
+              source={require('../../assets/img/petPointPurple.png')}
+              w={25}
+              h={25}
+              resizeMode="contain"
+            />
+          </Marker>
           <MapViewDirections
             origin={pickupAndDropCords}
             destination={currentCords}
             apikey={process.env.GOOGLE_MAP_API_KEY}
+            strokeColor={mainColor}
+            strokeWidth={5}
           />
         </MapView>
 
