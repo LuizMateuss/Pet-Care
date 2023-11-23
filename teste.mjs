aa90bfc2c8c0314f804069bdf3169f19b4915de2
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2023-11-23 01:35:23 -0300

    teste: action

--- teste.mjs
+++
@@ -1,9 +0,0 @@
-import { chdir, cwd } from 'node:process';
-
-console.log(`Starting directory: ${cwd()}`);
-try {
-  chdir('Mobile');
-  console.log(`New directory: ${cwd()}`);
-} catch (err) {
-  console.error(`chdir: ${err}`);
-}
